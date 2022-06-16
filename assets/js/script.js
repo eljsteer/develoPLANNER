6737010da46cdac4ea39f6709af3d942c6467cc5
Jason <el.jsteer@gmail.com>
2022-06-16 21:40:40 +1000

    small QOL changes to clean code and add comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,23 +6,9 @@ const timeStageEl = $(".time-stage");
 const saveBtn = $(".saveBtn");
 const saveDisplay = $("#data-saved");
 let currentHour = today.format("kk");
-let savedSchedules = [];
 
-const hours = [
-    08,
-    09,
-    10,
-    11,
-    12,
-    13,
-    14,
-    15,
-    16,
-    17,
-]
-
+// Function which loops through each data-attribute in the HTML on each element to check whether the currentHour time is in past, present or future and applies a class with css attributes.
 timeStageEl.each(function(index, item) {
-
     if (currentHour > item.dataset["time"]) {
      item.classList.add("past");
     
@@ -33,22 +19,22 @@ timeStageEl.each(function(index, item) {
         item.classList.add("present");
     };
 })
-
+// eventListener function that will save the schedules entered for the corresponding hour into localStorage to be used in later function 
 saveBtn.on("click", function(event) {
    
-    let text = $(this).siblings(".time-stage").val();
+    let scheduleText = $(this).siblings(".time-stage").val();
 
     let apptHour = $(this).siblings(".time-stage").attr("data-time");
 
-    localStorage.setItem(apptHour, (text))
+    localStorage.setItem(apptHour, (scheduleText))
 })
-
+// looping function which loops through data-attributes in HTML on each Textarea element to retrieve data from local storage.
 timeStageEl.each(function(index, item) {
-    var retrieve = $(this).attr("data-time");
-    var retrieveItem = localStorage.getItem(retrieve);
-    $(this).val(retrieveItem)
+    var scheduleData = $(this).attr("data-time");
+    var retrieveSchedule = localStorage.getItem(scheduleData);
+    $(this).val(retrieveSchedule)
  })
-
+// Function which controls the text popup on the click of the Savebutton to notify client schedule is saved.
 saveBtn.on("click", function () {
     var saveMessage = ("Appointment added to localStorage ✔")
     saveDisplay.text(saveMessage);
